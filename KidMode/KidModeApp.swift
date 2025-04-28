a34f2ba019789e1dffe97449ac45e2c2b467b50d
OlimpiaDev16 <168941945+OlimpiaDev16@users.noreply.github.com>
2025-04-28 16:19:43 -0400

    Initial Commit

---
+++ KidMode/KidModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidModeApp.swift
+//  KidMode
+//
+//  Created by Olimpia Compean on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct KidModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KidModeTests/KidModeTests.swift
@@ -0,0 +1,17 @@
+//
+//  KidModeTests.swift
+//  KidModeTests
+//
+//  Created by Olimpia Compean on 4/28/25.
+//
+
+import Testing
+@testable import KidMode
+
+struct KidModeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
